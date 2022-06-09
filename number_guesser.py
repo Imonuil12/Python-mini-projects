d290ad16f3946820fb5441a5b08fc2e884d4387c
Imonuil-Suleimanov <imasuleymanov@gmail.com>
2022-06-09 15:52:02 +0600

    All the features have beeen added

--- number_guesser.py
+++ number_guesser.py
@@ -13,14 +13,23 @@ else:
     quit()
         
 random_number= random.randint(0, top_of_range)
-print(random_number)
+guesses = 0
 
 while True:
+    guesses += 1
     user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
-    if top_of_range.isdigit():
-        top_of_range = int(top_of_range)
-    
     else:
         print('Please type a number next time.')
+        continue
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    else:
+        if user_guess > random_number:
+            print("You werer above the number!")
+        else:
+            print("You were below the number!")
+            
+print("You got it in", guesses, "guesses")
